80c6859c8dcf0346aac75f66d72bba76ef083ffb
ashishkyasari <ashish.akyasari@gmail.com>
2023-05-22 18:24:37 +0530

    first commit

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function SignUp() {
+    const [user, setUser] = useState({
+        name: "",
+        phoneno: "",
+        email: "",
+        password: "",
+    })
+
+    const handleChange = e => {
+        const { name, value, type } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+    const register = () => {
+        console.log(user);
+        user.phoneno = "9579203196"
+        console.log(user);
+        const { name, phoneno, email, password} = user
+        if (name && email && password && phoneno) {
+            axios.post("http://localhost:5001/register", user).then(res => {
+                alert(res.data.message)
+            })
+        } else {
+            alert("Invalid Input");
+        }
+    }
+
     return (
+        
         <Layout>
+            {
+                
+            }
             <div id="signup-container">
                 <form id="signup_form">
                     <h1 id="signup_heading">Sign Up</h1>
@@ -18,8 +52,10 @@ function SignUp() {
                         className="signup_input"
                         type="text"
                         name="name"
+                        value={user.name}
                         id="name"
                         placeholder="Enter Name"
+                        onChange={handleChange}
                     />
 
                     <label className="signup_label" htmlFor="phone">
@@ -27,11 +63,13 @@ function SignUp() {
                     </label>
                     <input
                         className="signup_input"
-                        type="tel"
-                        id="phone"
-                        name="phone"
+                        type="text"
+                        
+                        name="phoneno"
+                        value={user.phoneno}
                         placeholder="Enter Telephone"
-                        pattern="[0-9]{10}"
+                        
+                        onChange={handleChange}
                     />
 
                     <label className="signup_label" htmlFor="email">
@@ -41,8 +79,10 @@ function SignUp() {
                         className="signup_input"
                         type="email"
                         name="email"
+                        value={user.email}
                         id="email"
                         placeholder="Enter Email"
+                        onChange={handleChange}
                     />
 
                     <label className="signup_label" htmlFor="password">
@@ -52,12 +92,14 @@ function SignUp() {
                         className="signup_input"
                         type="password"
                         name="password"
+                        value={user.password}
                         id="password"
                         minLength="7"
                         placeholder="Enter Password"
+                        onChange={handleChange}
                     />
 
-                    <button className="signup_buttons">Sign Up</button>
+                    <button className="signup_buttons" onClick={register}>Sign Up</button>
 
                     <hr className="signup_hr" />
 
